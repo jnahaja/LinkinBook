745c011b6e0c4b462cd7f08f59fbe7b7140a27cc
jnahaja <jnahaja@gmail.com>
2016-05-24 09:16:57 -0400

    commit skeleton, gitignore should work

---
+++ LinkinBook/Source/LinkinBook.Helpers/Helpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkinBook.Helpers
+{
+    public class Helpers
+    {
+        public static string GetCurrentClientDirectoryName(string currentPath)
+        {
+            var directoryName = string.Empty;
+            var directory = new DirectoryInfo(currentPath ?? Directory.GetCurrentDirectory());
+            while (directory != null && !directory.GetFiles("*.sln").Any())
+            {
+                directory = directory.Parent;
+            }
+
+            if (Directory.Exists(Path.Combine(directory.FullName, "Source")))
+            {
+                directoryName = Path.Combine(directory.FullName, "Source");
+            }
+
+            return directoryName;
+        }
+    }
+}
